87c6421dcac7503605a60bd6fb1abff33829807d
nocheol <qkdl60@gmail.com>
2024-08-26 03:28:29 +0900

    feat : cartList 컴포넌트 추가

--- qkdl60/src/components/CartList.js
+++ qkdl60/src/components/CartList.js
@@ -2,16 +2,43 @@ import Component from './core/Component.js';
 
 export default class CartList extends Component {
   template() {
-    return ` <ul class="divide-y divide-gray-200">
-    ${this.state
-      .map(
-        ({ id, imgSrc, name, price, count }) => `
-          <li class="flex py-6" id=${id}>
+    return ` <section
+          class="pointer-events-auto w-screen max-w-md transition ease-in-out duration-500 translate-x-full"
+          id="shopping-cart"
+        >
+          <div
+            class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl"
+          >
+            <div class="flex-1 overflow-y-auto p-6">
+              <div class="flex items-start justify-between">
+                <h2 class="text-xl font-bold">장바구니</h2>
+                <div class="ml-3 flex h-7 items-center">
+                  <button
+                    type="button"
+                    class="-m-2 p-2 text-gray-400 hover:text-gray-500"
+                  >
+                    <svg
+                      id="close-cart-btn"
+                      class="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <!-- 아래 하드코딩 되어있는 장바구니 목록들을 유저 상호작용에 맞게 렌더링 되도록 변경해주세요. -->
+              <div id="cart-list">
+              <ul class="divide-y divide-gray-200">
+              ${this.state.list.map(
+                (item) => ` <li class="flex py-6" id=${item.id}>
                     <div
                       class="h-24 w-24 overflow-hidden rounded-md border border-gray-200"
                     >
                       <img
-                        src=${imgSrc}
+                        src=${item.imgSrc}
                         class="h-full w-full object-cover object-center"
                       />
                     </div>
@@ -20,14 +47,14 @@ export default class CartList extends Component {
                         <div
                           class="flex justify-between text-base font-medium text-gray-900"
                         >
-                          <h3>${name}</h3>
-                          <p class="ml-4">${price.toLocaleString()}원</p>
+                          <h3>${item.name}</h3>
+                          <p class="ml-4">${item.price.toLocaleString()}원</p>
                         </div>
                       </div>
                       <div class="flex flex-1 items-end justify-between">
                         <div class="flex text-gray-500">
                           <button class="decrease-btn">-</button>
-                          <div class="mx-2 font-bold">${count}개</div>
+                          <div class="mx-2 font-bold">${item.count}개</div>
                           <button class="increase-btn">+</button>
                         </div>
                         <button
@@ -39,8 +66,39 @@ export default class CartList extends Component {
                       </div>
                     </div>
                   </li>`
-      )
-      .join('')}
-    </ul>`;
+              )}
+              </ul>
+              </div>
+            </div>
+            <div class="border-t border-gray-200 p-6">
+              <div class="flex justify-between font-medium">
+                <p>결제금액</p>
+                <p class="font-bold" id="total-count">${this.state.total.toLocaleString()}원</p>
+              </div>
+              <a
+                id="payment-btn"
+                href="#"
+                class="flex items-center justify-center rounded-md border border-transparent bg-sky-400 px-6 py-3 mt-6 font-medium text-white shadow-sm hover:bg-sky-500"
+                >결제하기</a
+              >
+              <div
+                class="mt-6 flex justify-center text-center text-sm text-gray-500"
+              >
+                <p>
+                  또는
+                  <button
+                    type="button"
+                    class="font-medium text-sky-400 hover:text-sky-500"
+                  >
+                    쇼핑 계속하기
+                  </button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>`;
+  }
+  addEvent() {
+    this.$target.addEvent('click', () => {});
   }
 }
--- qkdl60/src/index.js
+++ qkdl60/src/index.js
@@ -1,1 +1,68 @@
 // 이 곳에 정답 코드를 작성해주세요.
+import CartList from './components/CartList.js';
+import ProductList from './components/ProductList.js';
+const productData = await fetch('./api/productData.json').then((res) =>
+  res.json()
+);
+let cartListData = { list: [], total: 0 };
+const $productList = document.querySelector('#product-list');
+new ProductList($productList, productData);
+
+const $cartList = document.querySelector('aside');
+
+const cartList = new CartList($cartList, cartListData);
+//TODO 이벤트 컴포넌틀 분리
+document.addEventListener('click', (event) => {
+  const $openCartBtn = event.target.closest('#open-cart-btn');
+  const $backDrop = document.querySelector('#backdrop');
+  const $closeBtn = document.querySelector('#close-cart-btn').closest('button');
+  const productId = event.target.getAttribute('data-productid');
+
+  //TODO벼튼 식별 방식
+  if (
+    $backDrop === event.target ||
+    event.target.closest('button') === $closeBtn
+  ) {
+    closeCart();
+    return;
+  }
+  if ($openCartBtn) {
+    openCart();
+    return;
+  }
+  if (productId !== null) {
+    const targetProduct = productData.find(
+      (product) => product.id == productId
+    );
+    const targetCartItem = cartListData.list.find(
+      (item) => item.id == productId
+    );
+
+    if (targetCartItem) {
+      targetCartItem.count++;
+    } else {
+      cartListData.list.push({ ...targetProduct, count: 1 });
+    }
+    cartListData.total = cartListData.list.reduce(
+      (acc, item) => acc + item.count * item.price,
+      0
+    );
+
+    cartList.setState(cartListData);
+    openCart();
+    return;
+  }
+});
+
+function openCart() {
+  const $backDrop = document.querySelector('#backdrop');
+  $backDrop.removeAttribute('hidden');
+  const $shoppingCart = document.querySelector('#shopping-cart');
+  $shoppingCart.classList.replace('translate-x-full', 'translate-x-0');
+}
+function closeCart() {
+  const $backDrop = document.querySelector('#backdrop');
+  $backDrop.setAttribute('hidden', true);
+  const $shoppingCart = document.querySelector('#shopping-cart');
+  $shoppingCart.classList.replace('translate-x-0', 'translate-x-full');
+}
